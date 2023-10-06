f7874a701d6b1fb6a936fcd19c330ec26762ef5e
DuyMaiBui` <duymaibui2k@gmail.com>
2023-10-06 22:52:15 +0700

    update scene service

--- Scripts/Services/Utils/SceneServices/SceneService.cs
+++ Scripts/Services/Utils/SceneServices/SceneService.cs
@@ -1,59 +1,42 @@
 namespace GameKit.Services.Utils.SceneServices
 {
-    using System.Collections.Generic;
-    using Cysharp.Threading.Tasks;
     using GameKit.Services.GameAsset;
+    using UnityEngine.ResourceManagement.AsyncOperations;
     using UnityEngine.ResourceManagement.ResourceProviders;
     using UnityEngine.SceneManagement;
 
     public interface ISceneService
     {
-        Dictionary<string, SceneInstance> NameToScene { get; }
-        UniTask<SceneInstance>            LoadSingleScene(string sceneName, bool activeOnLoad = true);
-        UniTask<SceneInstance>            LoadAdditiveScene(string sceneName, bool activeOnLoad = true);
-        UniTask                           UnloadScene(string sceneName);
+        AsyncOperationHandle<SceneInstance> LoadSingleScene(string sceneName, bool activeOnLoad = true);
+        AsyncOperationHandle<SceneInstance> LoadAdditiveScene(string sceneName, bool activeOnLoad = true);
+        AsyncOperationHandle                UnloadScene(string sceneName);
     }
 
     public class SceneService : ISceneService
     {
         #region Inject
 
-        private readonly IGameAssets assetServices;
+        private readonly IGameAssets gameAsset;
 
         #endregion
 
         public static SceneService Instance;
 
-        public SceneService(IGameAssets assetServices) { this.assetServices = assetServices; }
+        public SceneService(IGameAssets gameAsset) { this.gameAsset = gameAsset; }
 
-
-        public Dictionary<string, SceneInstance> NameToScene { get; set; } = new();
-
-        public async UniTask<SceneInstance> LoadSingleScene(string sceneName, bool activeOnLoad = true)
+        public AsyncOperationHandle<SceneInstance> LoadSingleScene(string sceneName, bool activeOnLoad = true)
         {
-            this.NameToScene.Clear();
-            var scene = await this.assetServices.LoadSceneAsync(sceneName, LoadSceneMode.Single, activeOnLoad);
-            this.NameToScene.Add(sceneName, scene);
-            return scene;
+            return this.gameAsset.LoadSceneAsync(sceneName, LoadSceneMode.Single, activeOnLoad);
         }
 
-        public async UniTask<SceneInstance> LoadAdditiveScene(string sceneName, bool activeOnLoad = true)
+        public AsyncOperationHandle<SceneInstance> LoadAdditiveScene(string sceneName, bool activeOnLoad = true)
         {
-            var scene = await this.assetServices.LoadSceneAsync(sceneName, LoadSceneMode.Additive, activeOnLoad);
-            this.NameToScene.Add(sceneName, scene);
-            return scene;
+            return this.gameAsset.LoadSceneAsync(sceneName, LoadSceneMode.Additive, activeOnLoad);
         }
 
-        public async UniTask UnloadScene(string sceneName)
+        public AsyncOperationHandle UnloadScene(string sceneName)
         {
-            if (!this.NameToScene.TryGetValue(sceneName, out var scene))
-            {
-                await SceneManager.UnloadSceneAsync(sceneName);
-                return;
-            }
-
-            await this.assetServices.UnloadSceneAsync(scene);
-            this.NameToScene.Remove(sceneName);
+            return this.gameAsset.UnloadSceneAsync(sceneName);
         }
     }
 }
